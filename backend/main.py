f8989d4ecac9a783c3801849ab275ca5ac70a641
Koshys <koshys.osu@gmail.com>
2024-01-11 14:41:53 +0200

    Page pagination
    Added endpoint for page pagination.
    Added example page to show this functionality

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 import base64
 import os
+from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import FastAPI, HTTPException, Depends, Request, File, UploadFile, Form
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Depends,
+    Query,
+    Request,
+    File,
+    UploadFile,
+    Form,
+)
 from datetime import datetime, timedelta
-from requests import Session
+from requests import Session, session
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from passlib.context import CryptContext
@@ -12,13 +22,13 @@ from dotenv import load_dotenv
 from models import User, MangaDB
 import jwt
 
-#this file must be refactored, after i done main job
+# this file must be refactored, after i done main job
 
 # Load environment variables from .env
 load_dotenv()
 
 # Load database URL from .env
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
 
@@ -41,16 +51,21 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 # Define JWT settings
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
+
 @app.post("/register/")
-def register_user(username: str = Form(...), email: str = Form(...), password: str = Form(...)):
+def register_user(
+    username: str = Form(...), email: str = Form(...), password: str = Form(...)
+):
     # Check if the username and email are unique
     session = SessionLocal()
-    existing_user = session.query(User).filter(
-        (User.username == username) | (User.email == email)
-    ).first()
+    existing_user = (
+        session.query(User)
+        .filter((User.username == username) | (User.email == email))  # type: ignore
+        .first()
+    )  # type: ignore
     if existing_user:
         raise HTTPException(
             status_code=400, detail="Username or email already registered"
@@ -60,35 +75,39 @@ def register_user(username: str = Form(...), email: str = Form(...), password: s
     hashed_password = pwd_context.hash(password)
 
     # Create a new user
-    new_user = User(username=username, email=email, hashed_password=hashed_password)
+    new_user = User(
+        username=username, email=email, hashed_password=hashed_password
+    )  # type: ignore
 
     session.add(new_user)
     session.commit()
     session.close()
     return {"message": "User registered successfully"}
 
+
 # Retrieve the user from the database by username
 @app.post("/login/")
 def login_user(request: Request, username: str = Form(...), password: str = Form(...)):
     session = SessionLocal()
-    user = session.query(User).filter(User.username == username).first()
+    user = session.query(User).filter(User.username == username).first()  # type: ignore
     session.close()
 
     if not user:
-        raise HTTPException(
-            status_code=401, detail="Username or password is incorrect"
-        )
+        raise HTTPException(status_code=401, detail="Username or password is incorrect")
 
     # Verify the password
     if not pwd_context.verify(password, user.hashed_password):
-        raise HTTPException(
-            status_code=401, detail="Username or password is incorrect"
-        )
-    
-    client_host = request.client.host
+        raise HTTPException(status_code=401, detail="Username or password is incorrect")
+
+    client_host = request.client.host  # type: ignore
     # Generate a JWT token based on user.id
     access_token = create_access_token(user.id)
-    return {"client_host": client_host, "access_token": access_token, "token_type": "bearer"}
+    return {
+        "client_host": client_host,
+        "access_token": access_token,
+        "token_type": "bearer",
+    }
+
 
 # Function to get the current user based on the JWT token
 def get_current_user(token: str = Depends(oauth2_scheme)):
@@ -98,19 +117,20 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         return user_id
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
-    except jwt.JWTError:
+    except jwt.JWTError:  # type: ignore
         raise HTTPException(status_code=401, detail="Could not validate token")
+
 
 # Endpoint to get user profile
 @app.get("/profile")
 def get_user_profile(current_user: int = Depends(get_current_user)):
     session = SessionLocal()
-    user = session.query(User).filter(User.id == current_user).first()
+    user = session.query(User).filter(User.id == current_user).first()  # type: ignore
     session.close()
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    
+
     # Convert the user object to a dictionary
     user_data = {
         "id": user.id,
@@ -118,19 +138,19 @@ def get_user_profile(current_user: int = Depends(get_current_user)):
         "email": user.email,
         # Add other fields as needed
     }
-    
+
     return user_data
 
-## manga section - refactor after
 
+# manga section - refactor after
 @app.post("/create_manga/")
 def create_manga(
-        name: str = Form(...),
-        description: str = Form(...),
-        tags: str = Form(...),
-        manga_type: str = Form(...),
-        cover_image: UploadFile = File(...),
-        current_user: int = Depends(get_current_user)
+    name: str = Form(...),
+    description: str = Form(...),
+    tags: str = Form(...),
+    manga_type: str = Form(...),
+    cover_image: UploadFile = File(...),
+    current_user: int = Depends(get_current_user),
 ):
     # Read the binary data of the image
     cover_data = cover_image.file.read()
@@ -143,18 +163,21 @@ def create_manga(
         tags=tags,
         manga_type=manga_type,
         cover=cover_data,
-    )
+    )  # type: ignore
     session.add(manga)
     session.commit()
     session.close()
 
-    return {"message": "Manga uploaded successfully"}    
+    return {"message": "Manga uploaded successfully"}
 
-# Endpoint for geying manga by id
+
+# Endpoint for geting manga by id
 @app.get("/manga/{manga_id}")
 def get_manga(manga_id: int):
     session = SessionLocal()
-    manga = session.query(MangaDB).filter(MangaDB.id == manga_id).first()
+    manga = (
+        session.query(MangaDB).filter(MangaDB.id == manga_id).first()  # type: ignore
+    )  # type: ignore
     session.close()
 
     if not manga:
@@ -167,10 +190,53 @@ def get_manga(manga_id: int):
         "description": manga.description,
         "tags": manga.get_tags(),
         "manga_type": manga.manga_type,
-        "cover_image": base64.b64encode(manga.cover).decode('utf-8') if manga.cover else None,
+        "cover_image": base64.b64encode(manga.cover).decode("utf-8")
+        if manga.cover
+        else None,
     }
 
     return manga_data
+
+# Endpoint to get the total number of manga
+@app.get("/manga_count")
+def get_total_manga_count():
+    session = SessionLocal()
+    total_count = session.query(MangaDB).count()
+    session.close()
+    return JSONResponse(content={"count": total_count})
+
+# get all manga by pagination, using offset(how many to skip) and limit(how many to display)
+@app.get("/manga")
+def get_manga_list(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, le=100)
+):
+    session = SessionLocal()
+
+    # Calculate the offset based on the page and limit
+    offset = (page - 1) * limit
+
+    # Query the database for manga with pagination
+    manga_list = session.query(MangaDB).offset(offset).limit(limit).all() # type: ignore
+
+    session.close()
+
+    # Convert the manga objects to a list of dictionaries
+    manga_data_list = []
+    for manga in manga_list:
+        manga_data = {
+            "id": manga.id,
+            "name": manga.name,
+            "description": manga.description,
+            "tags": manga.get_tags(),
+            "manga_type": manga.manga_type,
+            "cover_image": (
+                base64.b64encode(manga.cover).decode("utf-8") if manga.cover else None
+            ),
+        }
+        manga_data_list.append(manga_data)
+
+    return manga_data_list
 
 # Endpoint for updating manga by id
 @app.put("/update_manga/{manga_id}")
@@ -181,14 +247,16 @@ def update_manga(
     tags: str = Form(...),
     manga_type: str = Form(...),
     cover_image: UploadFile = File(...),
-    current_user: int = Depends(get_current_user)
+    current_user: int = Depends(get_current_user),
 ):
     # Read the binary data of the image
     cover_data = cover_image.file.read()
 
     # Update manga in the database
     session = SessionLocal()
-    manga = session.query(MangaDB).filter(MangaDB.id == manga_id).first()
+    manga = (
+        session.query(MangaDB).filter(MangaDB.id == manga_id).first()  # type: ignore
+    )  # type: ignore
 
     if not manga:
         raise HTTPException(status_code=404, detail="Manga not found")
@@ -205,11 +273,12 @@ def update_manga(
 
     return {"message": "Manga updated successfully"}
 
+
 # Create a JWT token with the user's username as the subject (sub)
 def create_access_token(user_id: int):
     payload = {
         "sub": user_id,
-        "exp": datetime.utcnow() + timedelta(days=30)  # Expiration time
+        "exp": datetime.utcnow() + timedelta(days=30),  # Expiration time
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
     return token
